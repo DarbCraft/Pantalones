58ce2c24445e2cf61ec43e9570199c6684b5bc8b
DarbCraft <lachie.darby@cewa.edu.au>
2024-08-02 21:02:31 +0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+document.getElementById('mummy').addEventListener('click', () => {
+    alert('SMELLY PANTS');
+});
+
+document.getElementById('daddy').addEventListener('click', () => {
+    alert('pantalones limpios');
+    setTimeout(() => {
+        alert('your pillow is not safe');
+    }, 500);
+});
+
+document.getElementById('felix').addEventListener('click', () => {
+    alert('pantalones limpios');
+});
+
+document.getElementById('lucy').addEventListener('click', () => {
+    alert('pantalones limpios');
+});
